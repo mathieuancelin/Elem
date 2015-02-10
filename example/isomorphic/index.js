c490076f516329fbfc4b988122212b5d0bd8648d
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2015-02-10 11:37:54 +0100

    Better isomorphic example

--- example/isomorphic/index.js
+++ example/isomorphic/index.js
@@ -1,62 +1,89 @@
+var express = require('express');
 var Elem = require('elemjs');
-
-var RenderOnlyTodoApp = Elem.el('div', { className: 'col-md-3' }, [
-    Elem.el('h3', 'Todo List'),
-    Elem.el('div',
-        Elem.el('div', { className: 'row' },
-            Elem.el('form', { role: 'form' }, [
-                Elem.el('div', { className: ['form-group', 'col-md-10'] },
-                    Elem.el('input', {
-                        placeholder: 'What do you have to do ?',
-                        type: 'text', className: 'form-control', value: '',
-                    }, [])
-                ),
-                Elem.el('div', { className: 'form-group' },
-                    Elem.el('div', { className: 'btn-group' }, [
-                        Elem.el('button', { type: 'button', className: 'btn btn-success' },
-                            Elem.el('span', {
-                                className: 'glyphicon glyphicon-floppy-saved'
-                            }, [])
-                        ),
-                        Elem.el('button', { type: 'button', className: 'btn btn-danger' },
-                            Elem.el('span', { className: 'glyphicon glyphicon-trash' }, [])
-                        )
-                    ])
-                )
-            ])
-        )
-    ),
-    Elem.el('ul', { className: 'list-group' }, [
-        Elem.el('li', { className: 'list-group-item' },
-            Elem.el('div', { className: 'row' }, [
-                Elem.el('div', { className: 'col-md-10' }, 'Buy some beer'),
-                Elem.el('div', { className: 'col-md-2' },
-                    Elem.el('span', { onclick: function() { alert('pouet'); }, className: { label: true, labelSuccess: true }, style: { cursor: 'pointer' } }, 'Done')
-                )
-            ])
-        ),
-        Elem.el('li', { className: 'list-group-item' },
-            Elem.el('div', { className: 'row' }, [
-                Elem.el('div', { className: 'col-md-10' }, 'Buy some pizza'),
-                Elem.el('div', { className: 'col-md-2' },
-                    Elem.el('span', { className: { label: true, labelDefault: true }, style: { cursor: 'pointer' } }, 'Done')
-                )
-            ])
-        )
-    ])
-]);
+var _ = Elem.Utils;
 
 var component = Elem.component({
-    init: function() {
+    init: function(state) {
         console.log("init ...");
+        state.set({
+            tasks: [
+                {
+                    name: 'Buy some beer',
+                    done: true
+                },
+                {
+                    name: 'Buy some pizza',
+                    done: false
+                }
+            ]
+        }, true);
     },
     afterRender: function() {
         console.log("after ...");
     },
     render: function(state, props) {
-        return RenderOnlyTodoApp;
+        return Elem.el('div', { className: 'col-md-4' }, [
+            Elem.el('h3', 'Todo List'),
+            Elem.el('div',
+                Elem.el('div', { className: 'row' },
+                    Elem.el('form', { role: 'form' }, [
+                        Elem.el('div', { className: ['form-group', 'col-md-10'] },
+                            Elem.el('input', {
+                                placeholder: 'What do you have to do ?',
+                                type: 'text', className: 'form-control', value: '',
+                            }, [])
+                        ),
+                        Elem.el('div', { className: 'form-group' },
+                            Elem.el('div', { className: 'btn-group' }, [
+                                Elem.el('button', { type: 'button', className: 'btn btn-success' },
+                                    Elem.el('span', {
+                                        className: 'glyphicon glyphicon-floppy-saved'
+                                    }, [])
+                                ),
+                                Elem.el('button', { type: 'button', className: 'btn btn-danger' },
+                                    Elem.el('span', { className: 'glyphicon glyphicon-trash' }, [])
+                                )
+                            ])
+                        )
+                    ])
+                )
+            ),
+            Elem.el('ul', { className: 'list-group' }, 
+                _.map(state().tasks, function(task) {
+                    return Elem.el('li', { className: 'list-group-item' },
+                        Elem.el('div', { className: 'row' }, [
+                            Elem.el('div', { className: 'col-md-10' }, task.name),
+                            Elem.el('div', { className: 'col-md-2' },
+                                Elem.el('span', { 
+                                    className: { 
+                                        label: true, 
+                                        labelDefault: !task.done, 
+                                        labelSuccess: task.done 
+                                    }, 
+                                    style: { 
+                                        cursor: 'pointer' 
+                                    } 
+                                }, 'Done')
+                            )
+                        ])
+                    ); 
+                })
+            )
+        ]);
     }
-})({});
+})();
 
 console.log(component.renderToString());
 console.log(component.renderToStaticHtml());
+
+var app = express();
+
+app.get('/app.html', function (req, res) {
+  res.send('<html><head><title>Isomorphic demo</title><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"><style type="text/css"></style></head><body><div id="app">' + component.renderToStaticHtml() + '</div></body></html>'); 
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Isomorphic app listening at http://%s:%s', host, port);
+});
